d4c25050e62d32994eb018170d5e0d7ed7327693
Richard <au.richard4@gmail.com>
2022-03-07 19:58:18 -0800

    Deleted #btn function that was from examples (served no purpose in code). Removed console logs.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-  console.log("Hello");
-
   $("#tweet-text").on("input", function (event) {
-    console.log("Current message", event.target.value);
     let charsRemain = 140 - event.target.value.length;
     $(".counter").text(charsRemain);
     if (charsRemain < 0) {
@@ -10,10 +7,6 @@ $(document).ready(function () {
     } else if (charsRemain >= 0) {
       $(".counter").css("color", "#c3c3c1");
     }
-  });
-
-  $("#btn").on("click", (event) => {
-    console.log(this, event.target); //The this keyword is a reference to the button
   });
 });
 
